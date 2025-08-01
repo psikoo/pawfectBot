e92f610e34bb40c00812eef6b0343e928cbc739f
psikoo <91342186+psikoo@users.noreply.github.com>
2025-08-01 19:31:54 +0200

    Update echo.js

--- src/commands/utility/echo.js
+++ src/commands/utility/echo.js
@@ -6,8 +6,8 @@ module.exports = {
 		.setDescription("Are we in a cave?")
 		.addStringOption(option => option
 			.setName("message")
-			.setDescription("Message to echo"))
-			.setRequired(true),
+			.setDescription("Message to echo")
+			.setRequired(true)),
 	async execute(interaction) {
 		await interaction.reply({ content: "Sending...", flags: MessageFlags.Ephemeral });
 		await interaction.channel.send(interaction.options.getString("message"));
