6973e0e94e90c988f41a92e0bb7356b8ad040614
psikoo <91342186+psikoo@users.noreply.github.com>
2025-08-05 01:01:14 +0200

    Update customRole.js

--- src/modal/customRole.js
+++ src/modal/customRole.js
@@ -4,16 +4,18 @@ require("dotenv").config()
 async function updateCustomRole(interaction) {
   const interactionUser = await interaction.guild.members.fetch(interaction.user.id)
   const colorRole = await interactionUser.roles.color;
-  let color1 = Number("0x"+interaction.fields.getTextInputValue("Color1"));
-  let color2 = (interaction.fields.getTextInputValue("Color2")) ? Number("0x"+interaction.fields.getTextInputValue("Color2")) : null;
+  const color1 = (interaction.fields.getTextInputValue("Color1") == "000000")? "010101" : interaction.fields.getTextInputValue("Color1");
+  const color2 = (interaction.fields.getTextInputValue("Color2")) ? interaction.fields.getTextInputValue("Color2") : null;
+  const hex1 = Number("0x"+color1);
+  const hex2 = (color2)? Number("0x"+color2) : null;
 
   // Update name
   colorRole.setName(interaction.fields.getTextInputValue("Name")+" ["+interaction.user.id+"]");
   // Update colors - support wil be added on discord.js 15
   let data = JSON.stringify({
     "colors": {
-      "primary_color": color1,
-      "secondary_color": color2,
+      "primary_color": hex1,
+      "secondary_color": hex2,
       "tertiary_color": null
     }
   });
