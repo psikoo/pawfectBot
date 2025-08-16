4523f0a80ea9dd6086852395b62bef7e59d98bba
psikoo <91342186+psikoo@users.noreply.github.com>
2025-08-16 03:02:00 +0200

    mute

--- src/commands/admin/mute.js
+++ src/commands/admin/mute.js
@@ -2,23 +2,20 @@ const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("disc
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("killfaggot")
-		.setDescription("Ban a user")
+		.setName("shutup")
+		.setDescription("Mute a user")
 		.addUserOption(option => option
 			.setName("user")
-			.setDescription("User to ban")
+			.setDescription("User to mute")
 			.setRequired(true))
-		.addBooleanOption(option => option
-			.setName("keep")
-			.setDescription("Keep the messages from the user"))
-		.addStringOption(option => option
-			.setName("reason")
-			.setDescription("Reason for the ban"))
+		.addNumberOption(option => option
+			.setName("time")
+			.setDescription("Time in minutes")
+			.setRequired(true))
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
 		let targetUser = interaction.options.getMember("user");
-		let keepMessages = interaction.options.getBoolean("keep");
-		let banReason = interaction.options.getString("reason");
+		let muteTime = interaction.options.getNumber("time");
 
 		if(targetUser === null) {
 			const response = new EmbedBuilder()
@@ -26,20 +23,18 @@ module.exports = {
 				.setTitle("🟥 Error")
 				.setDescription("🚫 This user is not part of this server!")
 			interaction.reply({embeds: [response]});
-		} else if(!targetUser.bannable) {
+		} else if(!targetUser.manageable) {
 			const response = new EmbedBuilder()
 				.setColor(0xFF0000)
 				.setTitle("🟥 Error")
-				.setDescription("🚫 Can't ban <@"+targetUser.user.id+"> missing permissions")
+				.setDescription("🚫 Can't mute <@"+targetUser.user.id+"> missing permissions")
 			interaction.reply({embeds: [response]});
 		} else {
-			if(keepMessages == true || !keepMessages) keepMessages = 0;
-			else keepMessages = 604800;
-			targetUser.ban({ reason: banReason, deleteMessageSeconds: keepMessages });
+			targetUser.timeout(muteTime*60*1000, "shush")
 			const response = new EmbedBuilder()
 				.setColor(0x00FF00)
 				.setTitle("🟩 Success")
-				.setDescription("⛔ banned <@"+targetUser.user.id+">, for "+banReason+".")
+				.setDescription("⛔ Muted <@"+targetUser.user.id+">")
 				.setImage("https://cdn.discordapp.com/attachments/1375288772987715735/1406076443200585780/image0.gif?ex=68a1263b&is=689fd4bb&hm=689150c9c45f67ccc590301d409c605c74da212934fc52045cf82c8c415f2407&")
 			interaction.reply({embeds: [response]});
 		}
