897eec04025786c0127891b71c1001af2909302a
psikoo <91342186+psikoo@users.noreply.github.com>
2025-09-03 15:42:35 +0200

    Update ban.js

--- src/commands/admin/ban.js
+++ src/commands/admin/ban.js
@@ -24,11 +24,13 @@ module.exports = {
 		let banReason = interaction.options.getString("reason");
 
 		if(targetUser === null) {
-			interaction.guild.members.ban(targetId);
+			if(keepMessages == true || !keepMessages) keepMessages = 0;
+			else keepMessages = 604800;
+			interaction.guild.members.ban(targetId, { reason: banReason, deleteMessageSeconds: keepMessages });
 			const response = new EmbedBuilder()
 				.setColor(0x00FF00)
 				.setTitle("🟩 Success")
-				.setDescription("⛔ banned <@"+targetUser.user.id+">, for "+banReason+".")
+				.setDescription("⛔ banned <@"+targetId+">, for "+banReason+".")
 				.setImage("https://cdn.discordapp.com/attachments/1375288772987715735/1406076443200585780/image0.gif?ex=68a1263b&is=689fd4bb&hm=689150c9c45f67ccc590301d409c605c74da212934fc52045cf82c8c415f2407&")
 			interaction.reply({embeds: [response]});
 		} else if(!targetUser.bannable) {
