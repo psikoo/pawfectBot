a3f7ec99244b7dfaa0bde9753551378c8c4d37ac
psikoo <91342186+psikoo@users.noreply.github.com>
2026-04-27 21:16:46 +0200

    Update sog.js

--- src/commands/utility/sog.js
+++ src/commands/utility/sog.js
@@ -7,10 +7,21 @@ module.exports = {
 		.addUserOption(option => option
 			.setName("user")
 			.setDescription("User to be sogged")
-			.setRequired(true)),
+			.setRequired(true))
+		.addStringOption(option => option
+			.setName("cat/dog")
+			.setDescription("choose between 'cat' or 'dog'")),
 	async execute(interaction) {
 		let targetUser = interaction.options.getMember("user");
+		let animal = interaction.options.getString("cat/dog");
 
-		await interaction.reply({ content: "<@"+targetUser.id+"> get sogged dummy!!", allowedMentions: { parse: [] } });
+		const dog = '[dog](https://tenor.com/view/water-spray-dog-hose-gif-5915950447361850547 "mwehehhe")';
+		const cat = '[cat](https://tenor.com/view/cat-doe-how-is-this-man-how-is-this-cat-he-be-starring-doe-wet-bath-gif-17193684 "mwehehhe")';
+		let reply = "<@"+targetUser.id+"> get sogged dummy!! "
+
+		if(animal == "dog") reply += dog;
+		if(animal == "cat") reply += cat;
+
+		await interaction.reply({ content: reply, allowedMentions: { parse: [] } });
 	},
 };
