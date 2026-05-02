b5d5ccaf53936672c5665af28faeadf4e0a38d54
psikoo <91342186+psikoo@users.noreply.github.com>
2026-05-02 15:53:19 +0200

    Update sog.js

--- src/commands/utility/sog.js
+++ src/commands/utility/sog.js
@@ -17,10 +17,12 @@ module.exports = {
 
 		const dog = '[dog](https://tenor.com/view/water-spray-dog-hose-gif-5915950447361850547 "mwehehhe")';
 		const cat = '[cat](https://tenor.com/view/cat-doe-how-is-this-man-how-is-this-cat-he-be-starring-doe-wet-bath-gif-17193684 "mwehehhe")';
+		const owl = '[owl](https://tenor.com/view/wet-owl-gif-5658056727679848013 "mwehehhe")';
 		let reply = "<@"+targetUser.id+"> get sogged dummy!! "
 
 		if(animal == "dog") reply += dog;
 		if(animal == "cat") reply += cat;
+		if(animal == "owl") reply += owl;
 
 		await interaction.reply({ content: reply, allowedMentions: { parse: [] } });
 	},
